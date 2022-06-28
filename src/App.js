6a657672f9e995e0446e4e7e28718eee1ec5d269
suhbatillo <ikromiddinov77@gmail.com>
2022-06-28 18:28:41 +0500

    kfdkfdnkfd

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const name = "dunyo"
+  return (
+    <div className="App">
+     <h1>salom {name}</h1>
+    </div>
+  );
+}
+
+export default App;
